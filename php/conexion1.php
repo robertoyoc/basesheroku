ed8c78ffb9d20f5ef4edbafee87668a04f9d4c03
robertoyoc <a01172599@itesm.mx>
2017-06-19 22:19:43 -0500

    testing pg connect6

--- php/conexion1.php
+++ php/conexion1.php
@@ -2,9 +2,6 @@
 $dbopts = parse_url(getenv('DATABASE_URL'));
 
 $data = "host=".$dbopts["host"]." port=".$dbopts["port"]." dbname=".$dbopts["path"]." user=".$dbopts["user"]." password=".$dbopts["pass"];
-$dbconn = pg_connect($data)
-  or die("No se pudo conectar");
-  echo "Conectado con éxito<br>";
-pg_close($dbconn);
+echo $data;
 
 ?>
