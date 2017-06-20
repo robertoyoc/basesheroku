12262793af499f018eb6c39444645ff11576f1f3
robertoyoc <a01172599@itesm.mx>
2017-06-19 22:17:09 -0500

    testing pg connect4

--- php/conexion1.php
+++ php/conexion1.php
@@ -1,13 +1,6 @@
 <?php
 $dbopts = parse_url(getenv('DATABASE_URL'));
 
-      'driver'   => 'pgsql',
-                   'user' => $dbopts["user"],
-                   'password' => $dbopts["pass"],
-                   'host' => $dbopts["host"],
-                   'port' => $dbopts["port"],
-                   'dbname' => ltrim($dbopts["path"],'/')
-
 $data = "host=".$dbopts["host"]." port=".$dbopts["port"]." dbname=".$dbopts["path"]." user=".$dbopts["user"]." password=".$dbopts["pass"];
 $dbconn =pg_connect($data);
 or die("No se pudo conectar");
