64f05f1b4707f85b4ee1feb53f8f5d12d443546f
robertoyoc <a01172599@itesm.mx>
2017-06-19 23:11:07 -0500

    login funciona

--- php/conexion1.php
+++ php/conexion1.php
@@ -4,6 +4,5 @@ $dbopts = parse_url(getenv('DATABASE_URL'));
 $data = "host=".$dbopts["host"]." port=".$dbopts["port"]." dbname=". ltrim($dbopts["path"],'/')." user=".$dbopts["user"]." password=".$dbopts["pass"];
 $dbconn = pg_connect($data)
   or die("No se pudo conectar");
-pg_close($dbconn);
 
 ?>
