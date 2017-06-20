fecf57632b0c833b0af25411e18d0d04abf82a8d
robertoyoc <a01172599@itesm.mx>
2017-06-19 21:47:29 -0500

    change on login.php

--- php/login.php
+++ php/login.php
@@ -1,33 +1,4 @@
 <?php
+	echo "Hola";
 
-	require 'conexion.php';
-
-	$contr = $_POST['password'];
-	$user = $_POST['usuario'];
-
-	$pass = md5($contr);
-
-	$query = "select * FROM usuarios where usuario='$user'";
-
-	$enlace->real_query($query);
-	$resultado = $enlace->use_result();
-	
-	if($resultado){
-		
-		$fila = $resultado->fetch_assoc();
-
-		if($pass == $fila['contrasena']){
-			session_start();
-			$_SESSION['perfil'] = $fila['perfil'];
-			$_SESSION['usuario'] = $fila['usuario'];
-			die();
-		}else
-			die('Contraseña incorrecta');
-	}
-	else
-	{
-		die('Usuario no encontrado');
-	}
-
-mysqli_close($enlace);
 ?>
